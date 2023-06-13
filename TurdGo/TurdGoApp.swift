95e4653da89d875b22b2363dece76a35971a09f0
Kelly Bian <kellybian85@gmail.com>
2023-06-12 20:15:57 -0500

    Initial Commit

---
+++ TurdGo/TurdGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurdGoApp.swift
+//  TurdGo
+//
+//  Created by Kelly Bian on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TurdGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
