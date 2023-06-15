59d37881acd18364eba2d32baa6313fad69e653e
Stellafishe <stellafishes@gmail.com>
2023-06-15 11:33:48 -0500

    Kind of fixed it???????

--- TurdGo/views/comment/Persistence.swift
+++ TurdGo/views/comment/Persistence.swift
@@ -5,10 +5,10 @@
 //  Created by Scholar on 6/14/23.
 ////
 //
-//import CoreData
-//
-//struct PersistenceController {
-//    static let shared = PersistenceController()
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
 //
 ////    static var preview: PersistenceController = {
 ////        let result = PersistenceController(inMemory: true)
@@ -28,17 +28,17 @@
 ////        return result
 ////    }()
 //
-//    let container: NSPersistentContainer
-//
-//    init(inMemory: Bool = false) {
-//        container = NSPersistentContainer(name: "TurdGo")
-//        if inMemory {
-//            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-//        }
-//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-//            if let error = error as NSError? {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "TurdGo")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 //
 //                /*
 //                 Typical reasons for an error here include:
@@ -49,8 +49,8 @@
 //                 Check the error message to determine what the actual problem was.
 //                 */
 //                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        })
+            }
+        })
 //        container.viewContext.automaticallyMergesChangesFromParent = true
-//    }
-//}
+    }
+}
--- TurdGo/views/comment/ToDoItem.swift
+++ TurdGo/views/comment/ToDoItem.swift
@@ -7,16 +7,16 @@
 
 
 
-//class ToDoItem: Identifiable {
-//    var id = UUID()
-//    var title = ""
-////    var title : String
-//    var isImportant = false
-////    var isImportant : Bool
-//
-//    init(title: String, isImportant: Bool = false) {
-//            self.title = title
-//            self.isImportant = isImportant
-//        }
-//}
-//import Foundation
+class ToDoItem: Identifiable {
+    var id = UUID()
+    var title = ""
+//    var title : String
+    var isImportant = false
+//    var isImportant : Bool
+
+    init(title: String, isImportant: Bool = false) {
+            self.title = title
+            self.isImportant = isImportant
+        }
+}
+import Foundation
