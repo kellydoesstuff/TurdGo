2670f0a09d15ddec3cb16a44a46963908c06419e
Kelly Bian <kellybian85@gmail.com>
2023-06-15 12:18:23 -0500

    added comment

---
+++ TurdGo/Custom Fonts/comment/ContentViewTwo.swift
@@ -0,0 +1,85 @@
+//
+//  ContentViewTwo.swift
+//  TurdGo
+//
+//  Created by Scholar on 6/15/23.
+//
+
+import SwiftUI
+
+
+struct ContentViewTwo: View {
+    
+    @Environment(\.managedObjectContext) var context
+    
+    @FetchRequest(
+        entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+    
+    var toDoItems: FetchedResults<ToDo>
+    
+    
+    @State private var showNewTask = false
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Reviews")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                Button(action: {
+                    self.showNewTask = true
+                    
+                }){
+                    Text("Add review")
+                        .font(.caption)
+                    
+                }
+                
+            } // Hstack ends
+            .padding()
+            Spacer()
+            
+            List {
+                ForEach (toDoItems) { toDoItem in
+                  
+                    if toDoItem.isImportant == true {
+                        Text("♿︎ " + (toDoItem.title ?? "No title"))
+                    }
+                    else {
+                        Text(toDoItem.title ?? "No title")
+                    }
+                    
+                }
+                .onDelete(perform: deleteTask)
+            }
+            
+            //            .listStyle(.plain)
+            
+        } // Vstack ends
+        
+        if showNewTask {
+            NewToDoView(showNewTask: $showNewTask, title: "", isImportant: false)
+        }
+        
+    }
+    
+    private func deleteTask(offsets: IndexSet) {
+        withAnimation {
+            offsets.map {toDoItems[$0] }.forEach(context.delete)
+            
+            do {
+                try context.save()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    
+    
+    struct ContentViewTwo_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ TurdGo/Custom Fonts/comment/ToDoItem.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoItem.swift
+//  TurdGo
+//
+//  Created by Scholar on 6/15/23.
+//
+
+class ToDoItem: Identifiable {
+    var id = UUID()
+    var title = ""
+//    var title : String
+    var isImportant = false
+//    var isImportant : Bool
+
+    init(title: String, isImportant: Bool = false) {
+            self.title = title
+            self.isImportant = isImportant
+        }
+}
+import Foundation
